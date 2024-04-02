da356749b5ace5e36cac37927afb49c8e5f3f9a3
PaulLin1 <103342860+PaulLin1@users.noreply.github.com>
2024-04-02 11:11:17 -0400

    added 'no review found' popup

--- script.js
+++ script.js
@@ -9,7 +9,11 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     let review = request.review;
     
     if(review != null) {
-        review_html.innerText = 'Fake'
+        review_html.innerText = 'hi'
+
+    }
+    else {
+        review_html.innerText = 'No Review Found'
     }
 
 })
@@ -17,19 +21,24 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 // runs the scrapReviewFromPage function when the "detect" button is clicked
 reviewScrape.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+    let url = await tab.url;
 
     chrome.scripting.executeScript({
+        args: [url],
         target: {tabId: tab.id},
-        func: scrapeReviewFromPage,
+        func: scrapeReviewFromPage
     })
 })
 
 // scrapes the review page
 // THIS IS THE MAIN PART YOU NEED TO LOOK AT SANHA
-async function scrapeReviewFromPage() {
+async function scrapeReviewFromPage(url) {
     // the text of the review
-    let review = document.evaluate("//*[contains(concat(' ', @id, ' '), 'customer_review')]/div[4]/span/span", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.innerText
+    let review = null;
     
+    if(url.includes('https://www.amazon.com/gp/customer-reviews/')) {
+        review = document.evaluate("//*[contains(concat(' ', @id, ' '), 'customer_review')]/div[4]/span/span", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.innerText
+    }
     // here is where you send the post request to the api
     chrome.runtime.sendMessage({review});
 }
